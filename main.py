0ee822e1657b4495d6d5ca0fbb5cc85444e817ee
Daniel John D. Mahinay <danieljohnmahinay47@gmail.com>
2026-09-05 22:42:57 +0800

    Change line 1 on simulation branch

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 print("Lab 1: Version Control")
 print("Author: Daniel John D. Mahinay")
 print("Student ID: TUPM-26-4014")
+<<<<<<< HEAD
 print("mahal ko si james manio james manio james manio Feature Enabled")
+=======
+print("mahal ko si yao Feature Enabled")
+>>>>>>> simulation
